d8ea9a8de4acc884da0611e312c919a1d599dd90
juancotera <juancoteracastro@gmail.com>
2023-11-12 12:32:00 -0500

    iniciando con estilos

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let express = require('express');
-let app  = express();
+const express = require('express');
+const app  = express();
+const path = require('path');
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, console.log('esto fue exitoso'));
-app.get('/', function(rew, res){
-  res.send('Bienvenidos al sitio');
+
+app.get('/', (req, res) => {
+  let htmlPath = path.resolve(__dirname, './views/index.html');
+  res.sendfile(htmlPath);
 });
