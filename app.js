2b400142e6eb61f09d2bb23ffb19af88d7b9be52
juancotera <juancoteracastro@gmail.com>
2023-11-23 16:55:18 -0500

    frontend completo

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app  = express();
 const path = require('path');
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, console.log('esto fue exitoso'));
+app.listen(3030, console.log('esto fue exitoso'));
 
 app.get('/', (req, res) => {
   let htmlPath = path.resolve(__dirname, './views/index.html');
+  res.sendfile(htmlPath);
+});
+
+app.get('/register', (req, res) => {
+  let htmlPath = path.join(__dirname, './views/register.html');
+  res.sendfile(htmlPath);
+});
+
+app.get('/login', (req, res) => {
+  let htmlPath = path.join(__dirname, './views/login.html');
   res.sendfile(htmlPath);
 });
